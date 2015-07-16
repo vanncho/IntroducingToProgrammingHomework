6ffbfc2af3ae2762eabddda9cc07f06a1e67c8ab
vanncho <van_cho@abv.bg>
2015-07-16 19:25:27 +0100

    Introducing to Programming, first three tasks done, 01.HelloCSharp, 02.PrintMyName and 03.PrintNumbers.

---
+++ PrintMyName/PrintMyName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+//A program to print my Name and Last Name in C#
+class PrintMyName
+{
+    static void Main()
+    {
+        System.Console.Write("Enter your first name:");
+        string firstName = System.Console.ReadLine();
+        System.Console.Write("Enter your last name:");
+        string lastName = System.Console.ReadLine();
+        System.Console.WriteLine("Hello, {0} {1}", firstName, lastName);
+        System.Console.ReadLine();
+    }
+}
---
+++ PrintNumbers/PrintNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+//A program to print three numbers, each on a separated line
+    class Program
+{
+    static void Main()
+    {
+        int firstNumer = 1;
+        int secondNumer = 101;
+        int thirdNumber = 1001;
+        System.Console.WriteLine(firstNumer);
+        System.Console.WriteLine(secondNumer);
+        System.Console.WriteLine(thirdNumber);
+        System.Console.ReadLine();
+    }
+}
