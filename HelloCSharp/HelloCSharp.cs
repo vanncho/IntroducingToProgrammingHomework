b494a35b4c09eb7be31ebde8d66111ddf1f9805a
vanncho <van_cho@abv.bg>
2015-07-16 19:15:24 +0100

    Introducing To Programming, first three tasks!

---
+++ HelloCSharp/HelloCSharp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+// A "Hello, C#!" program in C#, my first program
+class HelloCSharp
+{
+    static void Main()
+    {
+        System.Console.WriteLine("Hello, C#!");
+        System.Console.WriteLine("And so Ivan, that was your first program!");
+    }
+}
+
