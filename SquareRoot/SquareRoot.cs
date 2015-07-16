8c238484f8fac8c2fef55ced52998c4b2568c361
vanncho <van_cho@abv.bg>
2015-07-16 19:37:44 +0100

    Added task04, Square Root of 12345 (Problem 8).

---
+++ SquareRoot/SquareRoot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+//A program that finds a sqare root of number 12345.
+
+    class SquareRoot
+{
+    static void Main()
+    {
+        int number = 12345;
+        System.Console.WriteLine(Math.Sqrt(number));
+    }
+}
